8b631f2378a7cb2371790fa128bf36b4d664e876
Sergii Bieliaievskyi <magelan09@gmail.com>
2023-07-29 13:26:15 +0200

    F1 explain

--- kubecui.sh
+++ kubecui.sh
@@ -41,18 +41,20 @@ __get_obj__(){
   export FZF_DEFAULT_COMMAND="kubectl get ${RS_TYPE} -n ${NAMESPACE:-default}"
   export FZF_DEFAULT_COMMAND_WIDE="${FZF_DEFAULT_COMMAND} -o wide"
   export -f node-shell
+  export -f __explain_obj__
+  export -f __prepare_explain__
   PARAMS=()
   case "$RS_TYPE" in
     node?(s) )
         PARAMS+=(--bind 'f2:execute:node-shell {1}')
-        HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F2 (shell) || F3 (YAML) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
+        HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F2 (shell) || F3 (YAML) || F5 (descr search) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
         ;;
     pod?(s) )
         PARAMS+=(--bind 'f2:execute:kubectl exec -it --namespace ${NAMESPACE:-default} {1} -- bash > /dev/tty')
-        HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F2 (shell) || F3 (YAML) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
+        HEADER='>> Scrolling: SHIFT - up/down || F1 (explain) || F2 (shell) || F3 (YAML) || F4 (edit) || F5 (descr search) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
         ;;
     *)
-      HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F3 (YAML) || F4 (edit) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
+      HEADER='>> Scrolling: SHIFT - up/down || F1 (explain) || F3 (YAML) || F4 (edit) || F5 (descr search) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
       ;;
   esac
   fzf --layout=reverse -m --header-lines=1 --info=inline \
@@ -65,10 +67,51 @@ __get_obj__(){
     --bind 'f1:execute:kubectl -n ${NAMESPACE:-default} describe $RS_TYPE {1} | less' \
     --bind 'f4:execute:kubectl -n ${NAMESPACE:-default} edit $RS_TYPE {1}' \
     --bind 'f8:execute:kubectl delete ${RS_TYPE} {1} --namespace ${NAMESPACE}' \
+    --bind 'f1:execute:__explain_obj__ ${RS_TYPE}' \
     --bind 'f3:execute:kubectl -n ${NAMESPACE:-default} get $RS_TYPE {1} -o yaml | less' \
     "${PARAMS[@]}" \
     --bind 'enter:accept' \
     --preview 'kubectl -n ${NAMESPACE:-default} describe $RS_TYPE {1}'
+}
+
+__explain__(){
+  export FZF_DEFAULT_COMMAND="kubectl api-resources"
+  fzf --layout=reverse --header-lines=1 --info=inline \
+    --prompt "CL: $(kubectl config current-context | sed 's/-context$//') NS: $(kubectl config get-contexts | grep "*" | awk '{print $5}')> " \
+    --header $'>> Scrolling: SHIFT - up/down || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) || Ctrl-f (search word) <<\n\n' \
+    --preview-window=right:50% \
+    --bind 'ctrl-/:change-preview-window(70%|40%|50%)' \
+    --bind 'enter:accept' \
+    --bind 'ctrl-r:reload:$FZF_DEFAULT_COMMAND' \
+    --bind 'ctrl-L:reload:$FZF_DEFAULT_COMMAND_WIDE' \
+    --bind 'ctrl-f:execute:kubectl describe $RS_TYPE {2} -n {1} | less' \
+    --preview 'kubectl explain {1}'
+}
+__explain_obj__(){
+  export RS_TYPE=$1
+  __prepare_explain__ $1 | fzf --layout=reverse --header-lines=1 --info=inline \
+    --prompt "CL: $(kubectl config current-context | sed 's/-context$//') NS: $(kubectl config get-contexts | grep "*" | awk '{print $5}')> " \
+    --header $'>> Scrolling: SHIFT - up/down || CTRL-/ (change view) Ctrl-f (search word) <<\n\n' \
+    --preview-window=right:70% \
+    --bind 'ctrl-/:change-preview-window(40%|50%|70%)' \
+    --bind 'enter:accept' \
+    --bind 'ctrl-f:execute:kubectl explain ${RS_TYPE}.{1} | less' \
+    --preview 'kubectl explain ${RS_TYPE}.{1}'
+}
+
+__prepare_explain__(){
+  export RS_TYPE=$1
+
+  EXPLAIN=$(kubectl explain ${RS_TYPE} --recursive | sed -r 's/FIELDS:/---/' | sed -n '\|---|,$p' | sed -r 's/(\w+)\t.*/\1:/g' | yq -o props -P . | sed -r 's/ =//g')
+
+  for line in $EXPLAIN; do
+    echo $line
+    ST=$line
+    for level in $(echo $line | sed -r 's/^([a-zA-Z\.]+)\.(\w+)$/\1/' | sed -r 's/\./ /g'); do
+      ST=$(echo $ST | sed -r 's/^([a-zA-Z\.]+)\.(\w+)$/\1/')
+      echo $ST
+    done
+  done | sort | uniq
 }
 
 __get_obj_all__(){
@@ -76,16 +119,18 @@ __get_obj_all__(){
   export FZF_DEFAULT_COMMAND="kubectl get $RS_TYPE -A"
   export FZF_DEFAULT_COMMAND_WIDE="${FZF_DEFAULT_COMMAND} -o wide"
   export -f tcp_port_pair
+  export -f __explain_obj__
+  export -f __prepare_explain__
   PARAMS=()
   case "$RS_TYPE" in
     pod?(s) )
         # PARAMS+=(--bind 'f8:execute:kubectl delete pod {2} --namespace {1}')
         PARAMS+=(--bind 'f6:execute:tcp_port_pair {1} {2}')
         PARAMS+=(--bind 'f2:execute:kubectl exec -it --namespace {1} {2} -- bash > /dev/tty')
-        HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F2 (shell) || F3 (YAML) || F6 (port-forward) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
+        HEADER='>> Scrolling: SHIFT - up/down || F1 (explain) || F3 (YAML) || F4 (edit) || F5 (descr search) || F6 (port-forward) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
         ;;
     *)
-      HEADER='>> Scrolling: SHIFT - up/down || F1 (descr search) || F3 (YAML) || F4 (edit) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
+      HEADER='>> Scrolling: SHIFT - up/down || F1 (explain) || F3 (YAML) || F4 (edit) || F5 (descr search) || F8 (delete) || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) <<\n\n'
       ;;
   esac
   fzf --layout=reverse -m --header-lines=1 --info=inline \
@@ -100,7 +145,8 @@ __get_obj_all__(){
     --bind 'ctrl-L:reload:$FZF_DEFAULT_COMMAND_WIDE' \
     "${PARAMS[@]}" \
     --bind 'f3:execute:kubectl get $RS_TYPE {2} -n {1} -o yaml | less' \
-    --bind 'f1:execute:kubectl describe $RS_TYPE {2} -n {1} | less' \
+    --bind 'f5:execute:kubectl describe $RS_TYPE {2} -n {1} | less' \
+    --bind 'f1:execute:__explain_obj__ ${RS_TYPE}' \
     --preview 'kubectl describe $RS_TYPE {2} -n {1}'
 }
 
@@ -127,46 +173,9 @@ __get_events_all__(){
     --bind 'ctrl-l:reload:$FZF_DEFAULT_COMMAND --sort-by=".lastTimestamp"' \
     --bind 'ctrl-k:reload:$FZF_DEFAULT_COMMAND --sort-by=".firstTimestamp"'
 }
-__explain__(){
-  export FZF_DEFAULT_COMMAND="kubectl api-resources"
-  fzf --layout=reverse --header-lines=1 --info=inline \
-    --prompt "CL: $(kubectl config current-context | sed 's/-context$//') NS: $(kubectl config get-contexts | grep "*" | awk '{print $5}')> " \
-    --header $'>> Scrolling: SHIFT - up/down || CTRL-/ (change view) || CTRL-R (refresh. omit -o wide) || Ctrl-L (-o wide) || Ctrl-f (search word) <<\n\n' \
-    --preview-window=right:50% \
-    --bind 'ctrl-/:change-preview-window(70%|40%|50%)' \
-    --bind 'enter:accept' \
-    --bind 'ctrl-r:reload:$FZF_DEFAULT_COMMAND' \
-    --bind 'ctrl-L:reload:$FZF_DEFAULT_COMMAND_WIDE' \
-    --bind 'ctrl-f:execute:kubectl describe $RS_TYPE {2} -n {1} | less' \
-    --preview 'kubectl explain {1}'
-}
 
-__prepare_explain__(){
-  export RS_TYPE=$1
 
-  EXPLAIN=$(kubectl explain ${RS_TYPE} --recursive | sed -r 's/FIELDS:/---/' | sed -n '\|---|,$p' | sed -r 's/(\w+)\t.*/\1:/g' | yq -o props -P . | sed -r 's/ =//g')
 
-  for line in $EXPLAIN; do
-    echo $line
-    ST=$line
-    for level in $(echo $line | sed -r 's/^([a-zA-Z\.]+)\.(\w+)$/\1/' | sed -r 's/\./ /g'); do
-      ST=$(echo $ST | sed -r 's/^([a-zA-Z\.]+)\.(\w+)$/\1/')
-      echo $ST
-    done
-  done | sort | uniq
-}
-
-__explain_obj__(){
-  export RS_TYPE=$1
-  __prepare_explain__ $1 | fzf --layout=reverse --header-lines=1 --info=inline \
-    --prompt "CL: $(kubectl config current-context | sed 's/-context$//') NS: $(kubectl config get-contexts | grep "*" | awk '{print $5}')> " \
-    --header $'>> Scrolling: SHIFT - up/down || CTRL-/ (change view) Ctrl-f (search word) <<\n\n' \
-    --preview-window=right:70% \
-    --bind 'ctrl-/:change-preview-window(40%|50%|70%)' \
-    --bind 'enter:accept' \
-    --bind 'ctrl-f:execute:kubectl explain ${RS_TYPE}.{1} | less' \
-    --preview 'kubectl explain ${RS_TYPE}.{1}'
-}
 
 k() {
   OBJ=$(echo "$@" | sed -r 's/^.*get[[:space:]](\w+[[:space:]]?[a-z]+[-0-9a-z]*)[[:space:]]?(-n)?.*$/\1/' | base64)
