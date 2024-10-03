fb85e1c91c119881485aaafa59575c2b9946d24c
Sergii Bieliaievskyi <magelan09@gmail.com>
2024-10-03 06:34:54 +0200

    deletion bug fix

--- fx/libs/confirm.sh
+++ fx/libs/confirm.sh
@@ -3,5 +3,5 @@
 #
 confirm()
 {
-  [ "$(echo -e "No\nYes ${1} the ${2}" | fzf --border=double --border-label="╢ Confirmation ╟" --margin 25% --prompt 'Are you sure? ')" == "Yes" ]
+  [[ "$(echo -e "No\nYes ${1} the ${2}" | fzf --border=double --border-label="╢ Confirmation ╟" --margin 25% --prompt 'Are you sure? ')" == Yes* ]]
 }
